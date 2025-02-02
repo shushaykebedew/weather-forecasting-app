a82c5835b8bf2793b5b8127a0bb39206e3ccbe69
shushaykebedew <shushaykebedew12@gmail.com>
2025-02-02 15:41:13 +0300

    .env file created for backend

--- backend/server.js
+++ backend/server.js
@@ -1,16 +1,16 @@
+require("dotenv").config();
 const express = require("express");
 const mysql = require("mysql");
 const cors = require("cors");
-
 const app = express();
 app.use(cors());
 app.use(express.json());
 
 const db = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "",
-  database: "weather-app",
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
 });
 
 app.post("/signup", (req, res) => {
